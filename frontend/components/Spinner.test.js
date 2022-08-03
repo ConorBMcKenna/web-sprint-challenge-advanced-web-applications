efe5c5d3a752c77a56567f0c6c35c5ad5009029e
Conor McKenna <conormckenna63@yahoo.com>
2022-08-03 13:28:00 -0600

    Tests created and passing

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,19 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Spinner from "./Spinner"
+
+const spinner = ()=>screen.queryByText("Please wait...")
+
+
+describe("Spinner", ()=>{
+  test("on state", ()=>{
+    render(<Spinner on={true} />)
+    expect(spinner()).toBeTruthy()
+  })
+  test("off state", ()=>{
+    render(<Spinner on={false} />)
+    expect(spinner()).toBeFalsy()
+  })
 })
